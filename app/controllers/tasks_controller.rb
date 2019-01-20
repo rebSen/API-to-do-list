fd0a0772d472aeff4cbfee049f05b6b6ad4ad16c
rebSen <archivesreb@gmail.com>
2019-01-21 00:09:51 +0100

    routes tasks ok except delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,40 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :update, :destroy]
+
+  # Get all tasks
   def index
+    @tasks = Task.all
+    render json: @tasks
   end
 
+
+  # Get /tasks/:id
   def show
+    render json: @task
   end
 
+  # Post a new task
   def create
+    @task = Task.create(title: params[:title], todo: params[:todo], done: params[:done] )
+    render json: @task
   end
 
+
+  # Put /tasks/:id
   def update
+    @task.update(title: params[:title], todo: params[:todo], done: params[:done] )
+    render json: @task
   end
 
+  # Route delete doesn't work 
   def delete
+    @task.destroy
+    render json: Task.all
   end
+
+  def set_task
+    @task =Task.find(params[:id])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/create'
-  get 'tasks/update'
-  get 'tasks/delete'
+  # resources :tasks, only: [:index, :show, :create]
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show'
+  post 'tasks', to: 'tasks#create'
+  put 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#delete'
+  # get 'tasks/show'
+  # get 'tasks/create'
+  # get 'tasks/update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
